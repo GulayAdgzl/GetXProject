5aef1816eb7862b0a2813258634a9775d91fb71e
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-07-17 19:27:57 +0300

    Fix initialRoute declaration in MyApp for consistency in main.dart

--- getx/lib/main.dart
+++ getx/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         GetPage(
             name: '/', page: () => const MyHomePage(), binding: HomeBinding()),
       ],
+      initialRoute: '/',
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
