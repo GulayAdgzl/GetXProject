faa672bf9c81f613b299c6089ebd78de5f8b5110
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-07-17 15:12:46 +0300

    Add Windows runner files for Flutter application
    - Created runner.exe.manifest to define application settings and DPI awareness.
    - Implemented utils.cpp and utils.h for console creation and command line argument handling.
    - Developed win32_window.cpp and win32_window.h to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ getx/lib/sayac_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class SayacController extends GetxController {
+  // Define your controller properties and methods here
+  var count = 0;
+
+  void increment() {
+    count++;
+  }
+
+  void decrement() {
+    if (count > 0) {
+      count--;
+    }
+  }
+}
