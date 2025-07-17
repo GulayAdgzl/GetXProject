10a0304f140fa6c66808992a4a7c42eb301ad797
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-07-17 20:17:29 +0300

    Add InitialBinding for SharedPref initialization before app starts

---
+++ getx/lib/core/initial_binding.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:getx/services/shared_pref.dart';
+
+class InitialBinding extends Bindings {
+  @override
+  void dependencies() {
+    // SharedPref servisini uygulama başlamadan önce kaydediyoruz.
+    Get.putAsync(() async {
+      final sharedPref = SharedPref();
+      await sharedPref.init();
+      return sharedPref;
+    });
+  }
+}
