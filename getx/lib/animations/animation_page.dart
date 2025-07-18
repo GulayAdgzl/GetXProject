7b12342a15a95f96f4501a4eb0c6e56e7e86ce7a
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-07-18 15:00:43 +0300

    Add AnimationPage class with initial setup for animation display

---
+++ getx/lib/animations/animation_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AnimationPage extends GetView<AnimationController> {
+  const AnimationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
